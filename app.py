f330ee78f5ea55916ac52f3e78ab305817220af6
José <josevmonteiro1903@gmail.com>
2026-06-13 16:49:13 -0300

    Todos os gráficos basicos plotados

--- app.py
+++ app.py
@@ -271,6 +271,133 @@ with aba1:
     )
 
     # =========================
+    # GRÁFICO 4
+    # =========================
+
+    st.subheader(
+        "Número de domicílios em risco de insegurança alimentar"
+    )
+
+    nivel_agrupamento4 = st.selectbox(
+        "Escolha o nível de visualização:",
+        ["Macro Região", "Micro Região", "Município"],
+        key="grafico_numero_domicilios"
+    )
+    nivel_recorte = st.selectbox(
+        "Escolha o nível de recorte social:",
+        ["Domicílios em risco de insegurança alimentar", "Domicílios em risco de insegurança alimentar com pessoas com deficiência", "Domicílios em risco de insegurança alimentar com pessoas em situação de rua", "Domicílios em risco de insegurança alimentar com pessoas de povo ou comunidade tradicional", "Domicílios em risco de insegurança alimentar com pessoas menores de 18 anos", "Domicílios em risco de insegurança alimentar com RF respondente do sexo feminino", "Domicílios em risco de insegurança alimentar com RF respondente do sexo masculino", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado pardo", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado branco", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado amarelo", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado preto", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado indígena"],
+        key="grafico_recorte"
+    )
+
+    if nivel_agrupamento4 == "Macro Região":
+        dados = df_macro
+
+    elif nivel_agrupamento4 == "Micro Região":
+        dados = df_micro
+
+    else:
+        dados = df_municipio
+    
+
+    agrupado_risco = (
+        dados
+        .groupby(nivel_agrupamento4, as_index=False)
+        [nivel_recorte]
+        .sum()
+    )
+
+    fig = px.bar(
+        agrupado_risco,
+        x=nivel_agrupamento4,
+        y=nivel_recorte,
+        text=nivel_recorte,
+        title=f"Pessoas em domicílios em risco de insegurança alimentar por {nivel_agrupamento3}"
+    )
+
+    fig.update_traces(
+        textposition="outside"
+    )
+
+    fig.update_layout(
+        xaxis_title=nivel_agrupamento4,
+        yaxis_title="Quantidade de Domicilios"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
+    # =========================
+    # GRÁFICO 5
+    # =========================
+    st.subheader("Percentual de domicílios em risco de insegurança alimentar")
+
+    nivel_agrupamento5 = st.selectbox(
+        "Escolha o nível de visualização:",
+        ["Macro Região", "Micro Região", "Município"],
+        key="grafico_porcentagem"
+    )
+
+    nivel_recorte2 = st.selectbox(
+        "Escolha o nível de visualização de recorte social:",
+        ["Domicílios em risco de insegurança alimentar", "Domicílios em risco de insegurança alimentar com pessoas com deficiência", "Domicílios em risco de insegurança alimentar com pessoas em situação de rua", "Domicílios em risco de insegurança alimentar com pessoas de povo ou comunidade tradicional", "Domicílios em risco de insegurança alimentar com pessoas menores de 18 anos", "Domicílios em risco de insegurança alimentar com RF respondente do sexo feminino", "Domicílios em risco de insegurança alimentar com RF respondente do sexo masculino", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado pardo", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado branco", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado amarelo", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado preto", "Domicílios em risco de insegurança alimentar com RF respondente autodeclarado indígena"],
+        key="grafico_recorte2"
+    )
+
+    
+
+    if nivel_agrupamento5 == "Macro Região":
+        dados = df_macro
+
+    elif nivel_agrupamento5 == "Micro Região":
+        dados = df_micro
+
+    else:
+        dados = df_municipio
+
+    agrupado_cobertura2 = (
+        dados
+        .groupby(nivel_agrupamento5, as_index=False)
+        [
+            [
+                nivel_recorte2,
+                "Domicílios com a TRIA aplicada"
+            ]
+        ]
+        .sum()
+    )
+
+    agrupado_cobertura2["% de insegurança alimentar"] = (
+        agrupado_cobertura2[nivel_recorte2]
+        /
+        agrupado_cobertura2["Domicílios com a TRIA aplicada"]
+    )*100
+
+    
+    fig = px.bar(
+        agrupado_cobertura2,
+        x=nivel_agrupamento5,
+        y="% de insegurança alimentar",
+        title=f"% {nivel_recorte2} por {nivel_agrupamento5}"
+    )
+
+    fig.update_traces(
+        texttemplate="%{y:.2f}%",
+        textposition="outside"
+    )
+
+    fig.update_layout(
+        xaxis_title=nivel_agrupamento5,
+        yaxis_title="Percentual de domicílios em risco de insegurança alimentar"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+    
+    # =========================
     # TABELA
     # =========================
 
